2b3d8c80a9e18c0e86c56319db03c2c522ebd27b
doraemoe <jinyifan@outlook.com>
2017-04-17 16:29:37 +0800

    win ffmpeg

---
+++ Assets/Scripts/FFmpegProcess/FFmpegProcessWindows.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_STANDALONE_WIN
+
+using System;
+using System.Diagnostics;
+
+using UnityEngine;
+
+namespace FP
+{
+	public class FFmpegProcessWindows : IFFmpegProcess
+	{
+		
+		public Process ConvertToOGG (string source, string destination)
+		{
+			var ffmpeg = Application.streamingAssetsPath + "/Encoder/ffmpeg.exe";
+			Process ffmpegProcess = new Process ();
+			ffmpegProcess.StartInfo.FileName = ffmpeg;
+			ffmpegProcess.StartInfo.Arguments = "-i " + source + " -vn -c:a libvorbis -q:a 5 " + destination + " -y";
+			ffmpegProcess.StartInfo.CreateNoWindow = true;
+			ffmpegProcess.StartInfo.UseShellExecute = false;
+			ffmpegProcess.StartInfo.RedirectStandardOutput = true;
+			ffmpegProcess.StartInfo.RedirectStandardError = true;
+
+			return ffmpegProcess;
+		}
+	}
+}
+
+#endif
